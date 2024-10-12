34e7717ccfb9e6c4fbb6eda39a4a04426733e576
Dandy Arya Akbar <dandyarya003@gmail.com>
2024-10-12 19:03:39 +0700

    Initial commit

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the index page!")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the about page!")
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the contacts page!")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/about", aboutHandler)
+	http.HandleFunc("/contact", contactHandler)
+	error := http.ListenAndServe(":8080", nil)
+	if error != nil {
+		fmt.Printf("An error has occured while starting the HTTP server: %s\n", error)
+		return;
+	}
+	fmt.Println("HTTP server has been successfully started and is running on port 8080.....")
+}
